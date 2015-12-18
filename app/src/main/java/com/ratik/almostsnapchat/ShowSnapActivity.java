dc5ad6e761cedbf606170154fd0cde8a64756d1a
Ratik Sharma <Ratik96@gmail.com>
2015-12-18 15:50:06 +0530

    Update image download method

--- app/src/main/java/com/ratik/almostsnapchat/ShowSnapActivity.java
+++ app/src/main/java/com/ratik/almostsnapchat/ShowSnapActivity.java
@@ -1,24 +1,22 @@
 package com.ratik.almostsnapchat;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.ImageView;
 
 import com.amazonaws.auth.CognitoCachingCredentialsProvider;
-import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener;
-import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
-import com.amazonaws.mobileconnectors.s3.transferutility.TransferState;
-import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 
-import java.io.File;
+import java.net.URL;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by Ratik on 18/12/15.
@@ -43,11 +41,9 @@ public class ShowSnapActivity extends AppCompatActivity {
     }
 
     private void downloadMedia() {
-        // Get the external storage directory
-        String appName = ShowSnapActivity.this.getString(R.string.app_name);
-        File mediaStorageDir = new File(
-                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
-                appName + "/" + key);
+
+        GeneratePresignedUrlRequest urlRequest = new GeneratePresignedUrlRequest(Constants.MY_BUCKET, key);
+        urlRequest.setExpiration(new Date(System.currentTimeMillis() + 10000));
 
         // Initialize the Amazon Cognito credentials provider
         CognitoCachingCredentialsProvider credentialsProvider =
@@ -58,32 +54,24 @@ public class ShowSnapActivity extends AppCompatActivity {
                 );
 
         AmazonS3 s3Client = new AmazonS3Client(credentialsProvider);
-        TransferUtility transferUtility = new TransferUtility(s3Client, getApplicationContext());
 
-        // Initiate the download
-        final TransferObserver observer = transferUtility.download(Constants.MY_BUCKET, key, mediaStorageDir);
-        observer.setTransferListener(new TransferListener() {
+        URL url = s3Client.generatePresignedUrl(urlRequest);
+        Picasso.with(this).load(url.toString()).into(imageView, new Callback() {
             @Override
-            public void onStateChanged(int id, TransferState state) {
-
+            public void onSuccess() {
+                Timer timer = new Timer();
+                timer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        finish();
+                    }
+                }, 10 * 1000);
             }
 
             @Override
-            public void onProgressChanged(int id, long bytesCurrent, long bytesTotal) {
-                int percentage = (int) (bytesCurrent / bytesTotal * 100);
-                Log.d(TAG, "Downloading: " + percentage + "%");
-                if(percentage == 100) {
-                    Bitmap bitmap = BitmapFactory.decodeFile(observer.getAbsoluteFilePath());
-                    imageView.setImageBitmap(bitmap);
-                }
-            }
-
-            @Override
-            public void onError(int id, Exception ex) {
+            public void onError() {
 
             }
         });
-
-
     }
 }
