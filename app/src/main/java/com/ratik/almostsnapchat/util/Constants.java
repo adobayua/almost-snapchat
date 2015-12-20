de6d4c7a42ea35b9b2eb52fe57e6194973fa525b
Ratik Sharma <Ratik96@gmail.com>
2015-12-20 16:02:10 +0530

    Add new Constants.java

---
+++ app/src/main/java/com/ratik/almostsnapchat/util/Constants.java
@@ -0,0 +1,11 @@
+package com.ratik.almostsnapchat.util;
+
+/**
+ * Created by Ratik on 20/12/15.
+ */
+public class Constants {
+    public static final String MY_BUCKET = "MY_BUCKET_ID";
+    public static final String POOL_ID = "MY_POOL_ID";
+
+    public static final int CONST_WRITE_EXTERNAL_STORAGE = 0;
+}
